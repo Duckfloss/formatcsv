d3ebef48282092a1fb33126ce270ae2493a23a9b
BenCJones <ben@bencjones.com>
2016-06-13 14:32:49 -0400

    Set source and destination files

--- lib/formatcsv.rb
+++ lib/formatcsv.rb
@@ -1,8 +1,28 @@
 #require 'formatcsv/cli'
 require 'pry'
-
+require 'yaml'
+require 'csv'
 
 module FormatCSV
+
+	class From
+
+		VENDORS = YAML.load(File.open("./formatcsv/vendors.yml"))
+
+		def initialize(source_file)
+			@source_file = CSV.read(source_file, :headers=>true,:header_converters=>:symbol)
+			@destination_file = "#{File.dirname(file)}/#{File.basename(file,'.csv')}_toShopify.csv"
+		end
+
+		def Uniteu
+			
+		end
+
+		def Rpro
+
+		end
+
+	end
 
 	class Write
 		def now
