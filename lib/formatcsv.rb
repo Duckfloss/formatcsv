9d91ad9e0ab6ba5bbe2f9f3b1b0889daa384a7b0
BenCJones <ben@bencjones.com>
2016-06-20 14:17:31 -0400

    target file parsing

--- lib/formatcsv.rb
+++ lib/formatcsv.rb
@@ -1,40 +1,60 @@
-#require 'formatcsv/cli'
-require 'pry'
-require 'yaml'
-require 'csv'
-require './lib/formatcsv/shopifile.rb'
-
 module FormatCSV
 
   class Formatter
+    attr_reader :source_file, :target_file, :verbose, :source, :data, :format, :products
 
-    attr_reader :source_file, :destination_file, :products
+    def initialize(options)
+      @source_file = CSV.read(options[:sourcefile],"r",:headers=>true,:header_converters=>:symbol)
 
-    def initialize(file)
-      @source_file = CSV.read(file, :headers=>true,:header_converters=>:symbol)
-      i = 0
-      while File.exist?("#{File.dirname(file)}/Shopify_export#{i}.csv") == true
-        i += 1
-      end
-      @destination_file = File.open("#{File.dirname(file)}/Shopify_export#{i}.csv","w")
+      # Get target file
+      @target_file = get_target_file({:source_file => options[:sourcefile], :target_file => options[:targetfile]})
+
+      # init empty products array
       @products = []
+
+      @source = options[:source]
+      @data = options[:data]
+      @format = options[:format]
+      @verbose = options[:verbose]
     end
 
     def inspect
     end
 
-    def uniteu
-      @source_file.each do |row|
-        @products << Shopifile.new(row).process
+    def get_target_file(params)
+      # If target_file is specified
+      if !params[:target_file].nil?
+        # If it doesn't contain a directory
+        if File.dirname(params[:target_file]) == "."
+          # put it in source_file base directory
+          target_file = "#{File.dirname(File.absolute_path(params[:source_file]))}/#{params[:target_file]}"
+        else
+          target_file = File.absolute_path(params[:target_file])
+        end
+        # then check if the file exists
+        if File.exist?(target_file)
+          # mark @merge true and open existing file
+          @merge = true
+          target_file = File.open(target_file, "r+")
+        else
+          # otherwise mark @merge false and create new file
+          @merge = false
+          target_file = File.new(target_file, "w")
+        end
+      else
+        # if target_file is NOT specified
+        # get directory of source file
+        path = File.dirname(File.absolute_path(params[:source_file]))
+        i = 1
+        target_file = "#{path}/#{File.basename(params[:source_file],'.csv')}#{i}.csv"
+        while File.exist?(target_file) == true
+          i += 1
+          target_file = "#{path}/#{File.basename(params[:source_file],'.csv')}#{i}.csv"
+        end
+        target_file = File.new(target_file, "w")
       end
-      @products
+      return target_file
     end
-
-    def rpro
-
-    end
-
   end
-
 
 end
