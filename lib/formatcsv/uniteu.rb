ae8959b8ffc6434d4abd0998d982569fdcef1c87
BenCJones <ben@bencjones.com>
2016-06-21 15:13:57 -0400

    Add source parsing

--- lib/formatcsv/uniteu.rb
+++ lib/formatcsv/uniteu.rb
@@ -1,10 +1,30 @@
-  class UniteU < FormatCSV::Formatter
+class UniteU < FormatCSV::Formatter
 
-    def uniteu
-      @source_file.each do |row|
-        @products << Shopifile.new(row).process
-      end
-      @products
+  def parse
+    case @format.downcase
+      when "shopify"
+        @source_file.each do |row|
+          @products << Shopifile.new(row, {:source=>"uniteu", :type=>"products"}).process
+        end
+#        if @merge == true
+          
+      when "dynalog"
+        @products = 'Dynalogger.new(row, {:source=>"uniteu", :type=>"products"}).process'
+      when "google"
+        @products = 'Googlefy.new(row, {:source=>"uniteu", :type=>"products"}).process'
+      else
+        @products = 'Something else'
     end
-
+    @products
   end
+
+  private
+
+  def write
+    
+  end
+
+  def merge(products)
+    
+  end
+end
